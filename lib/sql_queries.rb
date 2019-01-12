39b1d968ce5956967ba178b700e8d8fedb7dd72b
Daniel  Drumwright <danieldrumwright@gmail.com>
2019-01-12 22:22:55 +0000

    Automatically backed up by Learn

--- lib/sql_queries.rb
+++ lib/sql_queries.rb
@@ -35,5 +35,5 @@ end
 
 def selects_the_category_name_and_the_sum_total_of_the_all_its_pledges_for_the_books_category
   "SELECT projects.category, SUM(pledges.amount) FROM projects JOIN pledges ON projects.id = pledges.project_id
-    GROUP BY projects.category WHERE projects.category = 'books';" 
+    WHERE projects.category = 'books' GROUP BY projects.category ;" 
 end
