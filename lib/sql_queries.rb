13328165a0ff40d6557478f7ade19c3fe100ecb8
Daniel  Drumwright <danieldrumwright@gmail.com>
2019-01-12 22:13:43 +0000

    Automatically backed up by Learn

--- lib/sql_queries.rb
+++ lib/sql_queries.rb
@@ -13,7 +13,7 @@ def selects_the_titles_of_all_projects_and_their_pledge_amounts_alphabetized_by_
 
 def selects_the_user_name_age_and_pledge_amount_for_all_pledges_alphabetized_by_name
   "SELECT users.name, users.age, SUM(pledges.amount) FROM users JOIN pledges ON users.id = pledges.user_id
-    GROUP BY users.name;" 
+    GROUP BY users.name;"
   end
 
 def selects_the_titles_and_amount_over_goal_of_all_projects_that_have_met_their_funding_goal
@@ -30,8 +30,8 @@ def selects_user_names_and_amounts_of_all_pledges_grouped_by_name_then_orders_th
 end
 
 def selects_the_category_names_and_pledge_amounts_of_all_pledges_in_the_music_category
-"SELECT projects.category, pledges.amount FROM projects JOIN pledges projects.id = pledges.project_id
-  HAVING project.category = 'music'; "
+"SELECT projects.category, pledges.amount FROM projects JOIN pledges ON projects.id = pledges.project_id
+  WHERE projects.category = 'music';"
 end
 
 def selects_the_category_name_and_the_sum_total_of_the_all_its_pledges_for_the_books_category
